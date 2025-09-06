082453051969e96d7df950a0d9f663ab2e749d53
Yang-Boyun <boyunyang@naver.com>
2025-09-06 17:32:13 +0900

    initial

---
+++ src/main/java/com/safebuy/SafeBuyApplication.java
@@ -0,0 +1,14 @@
+package com.safebuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeBuyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SafeBuyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/safebuy/SafeBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safebuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeBuyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
